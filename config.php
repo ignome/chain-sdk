b3c1a712ab4cf8095664c9c0a22d5c0cb5e6450c
ignome <ignome@foxmail.com>
2022-07-12 15:26:51 +0800

    Add  修改配置

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'host' => env('CHAIN_SERVER_API', 'http://127.0.0.1'),//主机
-    'port' => '8901',//端口
-    'timeout' => 60,//请求超时时间
+    'host' => env('CHAIN_SERVER_HOST', 'http://127.0.0.1'),//主机
+    'port' => env('CHAIN_SERVER_PORT', 8801),//端口
+    'timeout' => env('CHAIN_SERVER_TIMEOUT', 60),//请求超时时间
 ];
